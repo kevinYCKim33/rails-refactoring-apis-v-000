d890c1e4ee35a3a8c74236d8f84f46e6c8dc9a69
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-03 15:22:40 -0800

    used github_service to create and get repos

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,16 +1,23 @@
 class RepositoriesController < ApplicationController
-  def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
-  end
-
-  # def index
-  #   github = GithubService.new({'access_token' => session[:token]})
-  #   @repos_array = github.get_repos
+  # def index ##test passing version
+  #   response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+  #   @repos_array = JSON.parse(response.body)
   # end
 
-  def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+  def index #version that actually uses the given #get_repos
+    github = GithubService.new({'access_token' => session[:token]})
+    @repos_array = github.get_repos
+  end
+
+  # def create ##test passing version
+  #   response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+  #   redirect_to '/'
+  # end
+
+  def create #version that actually uses the given #get_repos
+    github = GithubService.new({'access_token' => session[:token]})
+    github.create_repo(params[:name])
     redirect_to '/'
   end
+
 end
